539846dc8b623036f14a24d369068cff93af93ed
st1np <leozinxdzs@gmail.com>
2021-09-29 13:58:14 -0300

    Terminando o menu que não funcionava ao clicar nos botões

--- script.js
+++ script.js
@@ -7,23 +7,41 @@ var menuSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" vi
 
 var closeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></svg>'
 
-var menuOpen = false
+var menuOpen = true
+
+function openMenu() {
+    menu.innerHTML = closeSvg
+    menuOpen = false
+    document.body.style.overflow = 'hidden'
+
+    coursesLi.style= `z-index: 10; display: flex; position: absolute; margin-top: 20rem;`
+    supportLi.style= `z-index: 10; display: flex; position: absolute; margin-top: 30rem;`
+}
+
+function closeMenu() {
+    menu.innerHTML = menuSvg
+    menuOpen = true
+    document.body.style.overflow = 'inherit'
+
+    coursesLi.style = ''
+    supportLi.style = ''
+}
 
 menu.addEventListener('click', () => {
-    if (menuOpen == false) {
-        menu.innerHTML = closeSvg
-        menuOpen = true
-        document.body.style.overflow = 'hidden'
-
-        coursesLi.style= `z-index: 10; display: flex; position: absolute; margin-top: 10rem;`
-        supportLi.style= `z-index: 10; display: flex; position: absolute; margin-top: 20rem;`
-    } else {
-        menu.innerHTML = menuSvg
-        menuOpen = false
-        document.body.style.overflow = 'inherit'
-
-        coursesLi.style = ''
-        supportLi.style = ''
+    if (menuOpen == true) {
+        openMenu()
+    } else if (menuOpen == false) {
+        closeMenu()
     }
+    menuToggle.classList.toggle('menu-toggle')
+})
+
+supportLi.addEventListener('click', () => {
+    closeMenu()
+    menuToggle.classList.toggle('menu-toggle')
+})
+
+coursesLi.addEventListener('click', () => {
+    closeMenu()
     menuToggle.classList.toggle('menu-toggle')
 })
